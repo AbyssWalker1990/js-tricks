ad11c80d45473e3fca63f8cdf1d4c5ccc85eff0a
AbyssWalker1990 <bordiian.v@gmail.com>
2023-03-18 11:59:44 +0200

    added notes to docker and basics

--- functions/callback/callback.js
+++ functions/callback/callback.js
@@ -4,4 +4,14 @@ function toggle () {
   button.classList.toggle('altColor')
 }
 
+// Example 1
 button.addEventListener('click', toggle)
+
+// Example 2
+button.addEventListener('click', function () {
+  button.classList.toggle('altColor')
+})
+
+//example 3
+button.addEventListener('click', () => { button.classList.toggle('altColor') })
+
