13ecc718d10f6bbf55323746c042371c5a5b82b3
Vova <bordiian.v@gmail.com>
2023-03-28 22:23:03 +0300

    symbol

--- objects/destructuringObj.js
+++ objects/destructuringObj.js
@@ -6,7 +6,12 @@ const user = {
 
 const { myName, age, hobbies: [firstHobby, secondHobby] } = user
 
+
+
 console.log(myName)
 console.log(age)
 console.log(firstHobby)
 console.log(secondHobby)
+
+const andriy = [3, 5, 7]
+andriy.push(10)
---
+++ test.js
@@ -0,0 +1,7 @@
+let name1 = 'Hello World, bye World'
+const name2 = name1.replaceAll('World', 'Universe')
+
+console.log(name2)
+
+
+
