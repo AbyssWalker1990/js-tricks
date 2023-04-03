b75dcb91fcf5b49b181ebfe48043d83f9e747e0e
Vova <bordiian.v@gmail.com>
2023-04-03 21:31:39 +0300

    added an example of wrapper for normal func to make it curried

--- functions/currying/convertFixed.js
+++ functions/currying/convertFixed.js
@@ -1,1 +1,14 @@
 // requires a func with a fixed number of parameters
+const curry = (fn) => {
+  return curried = (...args) => {
+    if (fn.length !== args.length) {
+      return curried.bind(null, ...args) // bind creates new func
+    }
+    return fn(...args)
+  }
+}
+
+const total = (x, y, z) => x + y + z
+
+const curriedTotal = curry(total)
+console.log(curriedTotal(10)(20)(30))
