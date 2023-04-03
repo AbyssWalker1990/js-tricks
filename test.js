d0fb5c104646617dd02b8c47b07cb557d7b1dacf
Vova <bordiian.v@gmail.com>
2023-04-03 13:28:03 +0300

    added info about SOLID

--- test.js
+++ test.js
@@ -1,13 +1,36 @@
+/**
+ * Complete the solution so that it reverses all of the words within 
+ * the string passed in.
 
-let name1 = 'Hello World, bye World'
-const name2 = name1.replaceAll('World', 'Universe')
+Words are separated by exactly one space and there are no 
+leading or trailing spaces.
 
-console.log(name2)
+Example(Input --> Output):
 
+"The greatest victory is that which requires no battle" --> 
+"battle no requires which that is victory greatest The"
+ */
 
+// function reverseWords(str) {
+//     const arr = str.split(" ")
+//     const reversedArr = arr.reverse()
+//     const strFromArr = reversedArr.join(' ')
+//     return strFromArr
+// }
 
-function test () {
-  console.log(this)
+function reverseWords(str) {
+    const arr = str.split(" ")
+    const resultArr = []
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let word = arr[i]
+        resultArr.push(word)
+    }
+
+    return resultArr.join(' ')
 }
-test()
 
+const arrTest = ['Tania', 'Vova', 'Vadim', 'Arina']
+console.log(arrTest[3])
+
+
+console.log(reverseWords("The greatest victory is that which"))
