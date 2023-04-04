4d2a0eeaecad3c6161663b1aa113d9b94e760b6c
AbyssWalker1990 <bordiian.v@gmail.com>
2023-04-04 13:14:24 +0300

    added info about generator functions

--- functions/generators/cardDeck.js
+++ functions/generators/cardDeck.js
@@ -1,1 +1,24 @@
-♠♣♡♢
+
+cardDeck = ({
+  suits: ['♠', '♣', '♡', '♢'],
+  court: ['J', 'Q', 'K', 'A'],
+  [Symbol.iterator]: function* () {
+    for (let suit of this.suits) {
+      for (let i = 2; i <= 10; i++) yield suit + i
+      for (let c of this.court) yield suit + c
+    }
+  }
+})
+
+console.log(...cardDeck)
+
+const myObject = {
+  data: [1, 2, 3, 4],
+  *[Symbol.iterator] () {
+    for (const value of this.data) {
+      yield value
+    }
+  }
+}
+
+console.log(...myObject)
