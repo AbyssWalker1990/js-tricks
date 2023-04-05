6931af3fdab085bc7fd93e5c44a5a05a6da0d6b0
Vova <bordiian.v@gmail.com>
2023-04-05 14:59:37 +0300

    added foreword to cleancode

--- test.js
+++ test.js
@@ -1,36 +1,5 @@
-/**
- * Complete the solution so that it reverses all of the words within 
- * the string passed in.
-
-Words are separated by exactly one space and there are no 
-leading or trailing spaces.
-
-Example(Input --> Output):
-
-"The greatest victory is that which requires no battle" --> 
-"battle no requires which that is victory greatest The"
- */
-
-// function reverseWords(str) {
-//     const arr = str.split(" ")
-//     const reversedArr = arr.reverse()
-//     const strFromArr = reversedArr.join(' ')
-//     return strFromArr
-// }
-
-function reverseWords(str) {
-    const arr = str.split(" ")
-    const resultArr = []
-    for (let i = arr.length - 1; i >= 0; i--) {
-        let word = arr[i]
-        resultArr.push(word)
-    }
-
-    return resultArr.join(' ')
-}
-
-const arrTest = ['Tania', 'Vova', 'Vadim', 'Arina']
-console.log(arrTest[3])
-
-
-console.log(reverseWords("The greatest victory is that which"))
+function parseF(s) {
+    return isNaN(parseFloat(s)) ? null : parseFloat(s);
+  }
+console.log(parseF('one'))
+console.log(parseF('1'))
