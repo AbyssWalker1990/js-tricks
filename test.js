51464c689cbaf3435aa92142f3a676c7e267b50a
Vova <bordiian.v@gmail.com>
2023-04-10 22:41:56 +0300

    chapter 2 finished

--- test.js
+++ test.js
@@ -1,5 +1,35 @@
-function parseF(s) {
-    return isNaN(parseFloat(s)) ? null : parseFloat(s);
+const romanNumerals = new Map()
+romanNumerals.set(1, 'I').set(2, 'II').set(3,'III').set(4,'IV')
+console.log(romanNumerals)
+const arr = [...romanNumerals]
+const arr2 = [...romanNumerals]
+const arr3 = [...romanNumerals]
+
+function concatArrays (arr) {
+  const result = []
+  for (item of arr) {
+    for (subItem of item) {
+      result.push(subItem)
+    }
   }
-console.log(parseF('one'))
-console.log(parseF('1'))
+  return result
+}
+const concated = concatArrays(arr)
+const concated1 = concatArrays(arr2)
+const concated2 = concatArrays(arr3)
+console.log(concated)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
