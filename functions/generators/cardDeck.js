46002975feef7a848bbd76f0a2953dd99a0edbbd
AbyssWalker1990 <bordiian.v@gmail.com>
2023-04-11 11:03:51 +0300

    created a class based Symbol iterator method for creating chessfigures

--- functions/generators/cardDeck.js
+++ functions/generators/cardDeck.js
@@ -12,3 +12,32 @@ cardDeck = {
 
 console.log(...cardDeck)
 
+
+
+
+
+class ChessFigures {
+  constructor(color) {
+    this.color = color
+    this.chess = 'chess'
+    this.king = 'king'
+    this.queen = 'queen'
+    this.tower = 'tower'
+    this.horse = 'horse'
+    this.officer = 'officer'
+  }
+
+  *[Symbol.iterator] () {
+    for (let i = 0; i < 10; i++) yield this.chess + ' ' + this.color
+    for (let i = 0; i < 2; i++) {
+      yield this.tower + ' ' + this.color
+      yield this.horse + ' ' + this.color
+      yield this.officer + ' ' + this.color
+    }
+    yield this.king + ' ' + this.color
+    yield this.queen + ' ' + this.color
+  }
+}
+
+const deckArr = new ChessFigures('black')
+console.log([...deckArr])
