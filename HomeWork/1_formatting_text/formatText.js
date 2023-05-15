ce342eee7b048014c51a8ef066c01be3bb126e51
Vova <bordiian.v@gmail.com>
2023-05-15 22:54:34 +0300

    done with refactor formattext

--- HomeWork/1_formatting_text/formatText.js
+++ HomeWork/1_formatting_text/formatText.js
@@ -1,2 +1,33 @@
-const a = Number('.')
-console.log(Number.isInteger(a))
+const path = require('path')
+const fsPromises = require('fs').promises
+
+const countTotalSumOfSymbols = async (dirPath) => {
+  const fileNames = await getFileNames(dirPath)
+  let sumArr = 0
+  for ( let fileName of fileNames) {
+    let result = await countSumOfSymbols(fileName)
+    sumArr += result
+  }
+  console.log(sumArr)
+}
+
+const getFileNames = async function (dirPath) {
+  const fileNameArr = await fsPromises.readdir(dirPath)
+  return fileNameArr
+}
+
+const countSumOfSymbols = async (inputFile) => {
+  const filePath = path.join(__dirname, 'data', inputFile)
+  const data = await fsPromises.readFile(filePath, 'utf-8')
+  const formattedData = data.replaceAll(' ', '')
+  const symbolsArr = formattedData.split('')
+  const onlyNums = symbolsArr.filter(symbol => Number.isInteger(Number(symbol)))
+  let sum = 0
+  onlyNums.forEach( num => {
+    sum += Number(num)
+  })
+  return sum 
+}
+
+
+countTotalSumOfSymbols(path.join(__dirname, 'data'))
