b780bdd48660655fb5058ef1bb2b7bef6f959c2e
Vova <bordiian.v@gmail.com>
2023-05-24 21:00:30 +0300

    added solution to reverse linked list

--- Javascript/linked-lists/reverseList.js
+++ Javascript/linked-lists/reverseList.js
@@ -13,3 +13,33 @@ const d = new Node('D')
 a.next = b
 b.next = c
 c.next = d
+
+const reverseList = (head) => {
+  let current = head
+  let prev = null
+  while (current !== null) {
+    const next = current.next
+    current.next = prev
+    prev = current
+    current = next
+  }
+  return prev
+}
+
+const showList = (head) => {
+  let current = head
+  while (current !== null) {
+    console.log(current.value)
+    current = current.next
+  }
+}
+
+reverseList(a)
+console.log(showList(d))
+
+const reverseListRecursive = (head, prev=null) => {
+  if (head === null) return prev
+  const next = head.next
+  head.next = prev
+  reverseListRecursive(next, head)
+}
