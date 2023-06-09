0b997a038de2e6bb598856faa16a6293e26af4bf
Vova <bordiian.v@gmail.com>
2023-06-09 10:01:09 +0300

    solved island count problem graph grid

--- Javascript/graph/islandCount.js
+++ Javascript/graph/islandCount.js
@@ -9,11 +9,37 @@ const grid = [
 
 
 const islandCount = (grid) => {
+  const visited = new Set()
+  let count = 0
   for (let r = 0; r < grid.length; r++) {
-    for (let c = 0; c < 5; c ++) {
-      console.log(grid[r][c])
+    for (let c = 0; c < grid[0].length; c++) {
+      if (explore(grid, r, c, visited) === true) {
+        count++
+      }
     }
   }
+  return count
 }
 
-islandCount(grid)
+
+
+const explore = (grid, r, c, visited) => {
+  const rowInbounds = 0 <= r && r < grid.length
+  const colInbounds = 0 <= c && c < grid[0].length
+  if (!rowInbounds || !colInbounds) return false
+
+  if (grid[r][c] === 'W') return false
+
+  const pos = r + ',' + c
+  if (visited.has(pos)) return false
+  visited.add(pos)
+
+  explore(grid, r - 1, c, visited)
+  explore(grid, r + 1, c, visited)
+  explore(grid, r, c - 1, visited)
+  explore(grid, r, c + 1, visited)
+
+  return true
+}
+
+console.log(islandCount(grid))
